b2d3fd9e6a14777df42fbb2052f51068e7eaa4b5
yohannesTz <yohannes222ethiopia@gmail.com>
2023-09-21 02:25:32 +0300

    expermenting with inline queries

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import { Bot, Context, session, SessionFlavor } from "grammy";
+import {
+  Bot,
+  Context,
+  InlineKeyboard,
+  InlineQueryResultBuilder,
+  session,
+  SessionFlavor,
+} from "grammy";
+import { Menu } from "@grammyjs/menu";
 import dotenv from "dotenv";
 import { io } from "socket.io-client";
 import { MessageModel } from "./util/models";
 import { getRandomInternetColor } from "./util/helpers";
 import { FileFlavor, hydrateFiles } from "@grammyjs/files";
 import { v2 as cloudinary } from "cloudinary";
+import { InlineQueryResult } from "grammy/types";
 
 interface User {
   hexColor: string;
@@ -41,7 +50,7 @@ bot.api.config.use(hydrateFiles(bot.token));
 bot.use(
   session({
     initial: () => ({
-      users: new Map<number, User>()
+      users: new Map<number, User>(),
     }),
   })
 );
@@ -85,14 +94,100 @@ const connect = () => {
 };
 
 const socket = connect();
-
 console.log("is Connected: ", socket.connected);
+
+// Define the video interface
+interface Video {
+  title: string;
+  url: string;
+}
+
+// Sample list of videos
+const videos: Video[] = [
+  { title: "Baby Crying", url: "https://example.com/baby_crying" },
+  { title: "Funny Cats", url: "https://example.com/funny_cats" },
+  { title: "Dancing Dogs", url: "https://example.com/dancing_dogs" },
+];
+
+// Function to search videos by title
+function searchVideosByTitle(searchTerm: string): Video[] {
+  const regex = new RegExp(searchTerm, "i");
+  return videos.filter((video) => regex.test(video.title));
+}
+
+// Build a photo result.
+InlineQueryResultBuilder.photo("id-0", "https://grammy.dev/images/grammY.png");
+
+// Build a result that displays a photo but sends a text message.
+InlineQueryResultBuilder.photo(
+  "id-1",
+  "https://grammy.dev/images/grammY.png"
+).text("This text will be sent instead of the photo");
+
+// Build a text result.
+InlineQueryResultBuilder.article("id-2", "Inline Queries").text(
+  "Great inline query docs: grammy.dev/plugins/inline-query"
+);
+
+// Pass further options to the result.
+const keyboard = new InlineKeyboard().text("Aw yis", "call me back");
+InlineQueryResultBuilder.article("id-3", "Hit me", {
+  reply_markup: keyboard,
+}).text("Push my buttons");
+
+// Pass further options to the message content.
+InlineQueryResultBuilder.article("id-4", "Inline Queries").text(
+  "**Outstanding** docs: grammy.dev",
+  { parse_mode: "MarkdownV2" }
+);
+
+bot.inlineQuery(/best bot (framework|library)/, async (ctx) => {
+  // Create a single inline query result.
+  const result = InlineQueryResultBuilder.article(
+    "id:grammy-website",
+    "grammY",
+    {
+      reply_markup: new InlineKeyboard().url(
+        "grammY website",
+        "https://grammy.dev/"
+      ),
+    }
+  ).text(
+    `<b>grammY</b> is the best way to create your own Telegram bots.
+They even have a pretty website! 👇`,
+    { parse_mode: "HTML" }
+  );
+
+  // Answer the inline query.
+  await ctx.answerInlineQuery(
+    [result], // answer with result list
+    { cache_time: 30 * 24 * 3600 } // 30 days in seconds
+  );
+});
+
+// Return empty result list for other queries.
+bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
 
 bot.hears(/grug/i, async (ctx) => {
   //console.log(JSON.stringify(ctx, null, 2));
   await ctx.reply("grug is out! hunting... :(", {
     reply_to_message_id: ctx.msg.message_id,
   });
+});
+
+const menu = new Menu("movements")
+  .text("^", (ctx) => ctx.reply("Forward!"))
+  .row()
+  .text("<", (ctx) => ctx.reply("Left!"))
+  .text(">", (ctx) => ctx.reply("Right!"))
+  .row()
+  .text("v", (ctx) => ctx.reply("Backwards!"));
+
+bot.use(menu);
+
+bot.hears(/menu/i, async (ctx) => {
+  console.log("Menu incoming...");
+  await ctx.reply("Check out this menu:", { reply_markup: menu });
 });
 
 bot.on(":text", async (ctx) => {
@@ -116,7 +211,7 @@ bot.on(":text", async (ctx) => {
         username: message.from.username as string,
         isBot: message.from.is_bot,
         currentBadge: "normal",
-        color: ctx.session.users.get(message.from.id)?.hexColor ?? "#fff"
+        color: ctx.session.users.get(message.from.id)?.hexColor ?? "#fff",
       },
     };
 
@@ -157,7 +252,7 @@ bot.on(":animation", async (ctx: MyContext) => {
         username: message.from.username as string,
         isBot: message.from.is_bot,
         currentBadge: "normal",
-        color:  ctx.session.users.get(message.from.id)?.hexColor ?? "#fff"
+        color: ctx.session.users.get(message.from.id)?.hexColor ?? "#fff",
       },
     };
 
@@ -198,12 +293,11 @@ bot.on(":sticker", async (ctx: MyContext) => {
         username: message.from.username as string,
         isBot: message.from.is_bot,
         currentBadge: "normal",
-        color:  ctx.session.users.get(message.from.id)?.hexColor ?? "#fff"
+        color: ctx.session.users.get(message.from.id)?.hexColor ?? "#fff",
       },
     };
 
     socket.emit("message", stickerMessage);
-    //ctx.reply(`Sticker detected... result: ${uploadResult.url}`);
   }
 });
 
