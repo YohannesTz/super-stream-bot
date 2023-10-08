24c7e6f1df05661797c49d6c6d7e8743da2132b4
yohannesTz <yohannes222ethiopia@gmail.com>
2023-10-08 20:19:00 +0300

    added resource sample

--- api/util/resources.ts
+++ api/util/resources.ts
@@ -1,5 +1,11 @@
 import { ReactionContent } from "./models";
 
 export const reactions: ReactionContent[] = [
-  // add yours here
+  // add yours here. here is a sample
+  {
+    title: "Money show off",
+    keywords: "rich poor peasants pleb plebs peasants black lady money showoff",
+    slug: "money_show_off_black_lady.mp4",
+    by: "Community",
+  },
 ];
